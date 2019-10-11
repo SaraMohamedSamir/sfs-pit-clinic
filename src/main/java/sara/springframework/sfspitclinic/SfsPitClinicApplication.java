5b846dbcfd5b12d966c722ad8e7f79971bb04009
Sara Ahmed <sara.ahmed@irdeto.com>
2019-10-11 16:32:47 +0200

    generated the pit clinic initial project

---
+++ src/main/java/sara/springframework/sfspitclinic/SfsPitClinicApplication.java
@@ -0,0 +1,13 @@
+package sara.springframework.sfspitclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfsPitClinicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SfsPitClinicApplication.class, args);
+  }
+
+}
---
+++ src/test/java/sara/springframework/sfspitclinic/SfsPitClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package sara.springframework.sfspitclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfsPitClinicApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
